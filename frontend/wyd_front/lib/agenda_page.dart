0d12c034a62f526315b0b7f27fa04faaa15ad70c
RedDuality <giacomo_romanini2000@libero.it>
2024-04-03 16:58:38 +0200

    Commit made from VS Code Azure Static Web Apps

--- frontend/wyd_front/lib/agenda_page.dart
+++ frontend/wyd_front/lib/agenda_page.dart
@@ -16,75 +16,75 @@ class AgendaPage extends StatelessWidget {
       dataSource: _getDataSource(),
       allowedViews: const [CalendarView.week, CalendarView.month],
       allowDragAndDrop: true,
-      onTap: (CalendarTapDetails details) {
-        if (details.targetElement == CalendarElement.appointment ||
-            details.targetElement == CalendarElement.agenda) {
-          final Appointment appointmentDetails = details.appointments![0];
-
-          var subjectText = appointmentDetails.subject;
-          var dateText = DateFormat('MMMM dd, yyyy')
-              .format(appointmentDetails.startTime)
-              .toString();
-          var startTimeText = DateFormat('hh:mm a')
-              .format(appointmentDetails.startTime)
-              .toString();
-          var endTimeText = DateFormat('hh:mm a')
-              .format(appointmentDetails.endTime)
-              .toString();
-          var timeDetails = "";
-          if (appointmentDetails.isAllDay) {
-            timeDetails = 'All day';
-          } else {
-            timeDetails = '$startTimeText-$endTimeText';
-          }
-          showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: Text(subjectText),
-                  content: SizedBox(
-                    height: 80,
-                    child: Column(
-                      children: <Widget>[
-                        Row(
-                          children: <Widget>[
-                            Text(
-                              dateText,
-                              style: const TextStyle(
-                                fontWeight: FontWeight.w400,
-                                fontSize: 20,
-                              ),
-                            ),
-                          ],
-                        ),
-                        const Row(
-                          children: <Widget>[
-                            Text(''),
-                          ],
-                        ),
-                        Row(
-                          children: <Widget>[
-                            Text(timeDetails,
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.w400, fontSize: 15)),
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                  actions: <Widget>[
-                    TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Text('close'))
-                  ],
-                );
-              });
-        }
-      },
+      onTap: (details) => calendarTapped(details, context),
       //monthViewSettings: const MonthViewSettings(showAgenda: true),
     ));
+  }
+}
+
+void calendarTapped(CalendarTapDetails details, BuildContext context) {
+  if (details.targetElement == CalendarElement.appointment ||
+      details.targetElement == CalendarElement.agenda) {
+    final Appointment appointmentDetails = details.appointments![0];
+
+    var subjectText = appointmentDetails.subject;
+    var dateText = DateFormat('MMMM dd, yyyy')
+        .format(appointmentDetails.startTime)
+        .toString();
+    var startTimeText =
+        DateFormat('hh:mm a').format(appointmentDetails.startTime).toString();
+    var endTimeText =
+        DateFormat('hh:mm a').format(appointmentDetails.endTime).toString();
+    var timeDetails = "";
+    if (appointmentDetails.isAllDay) {
+      timeDetails = 'All day';
+    } else {
+      timeDetails = '$startTimeText-$endTimeText';
+    }
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text(subjectText),
+            content: SizedBox(
+              height: 80,
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Text(
+                        dateText,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Row(
+                    children: <Widget>[
+                      Text(''),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Text(timeDetails,
+                          style: const TextStyle(
+                              fontWeight: FontWeight.w400, fontSize: 15)),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('close'))
+            ],
+          );
+        });
   }
 }
 
