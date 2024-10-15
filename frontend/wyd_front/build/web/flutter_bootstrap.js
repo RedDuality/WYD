b21f69e2793965ecf0cde5e22f9f5a68746b470b
RedDuality <giacomo_romanini2000@libero.it>
2024-10-15 14:20:37 +0200

    flutter build web/apk

--- frontend/wyd_front/build/web/flutter_bootstrap.js
+++ frontend/wyd_front/build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "790820412"
+    serviceWorkerVersion: "1191197442"
   }
 });
