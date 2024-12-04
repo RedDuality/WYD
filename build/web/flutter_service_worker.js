b044118c741ce81e5d362269c99aea8e443d666f
RedDuality <giacomo_romanini2000@libero.it>
2024-12-04 20:17:19 +0100

    flutter build web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -27,14 +27,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "47acc57f1d47dbc5d7827db37bb2e128
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "a45dc865b753a7bf9da32eb1009761e7",
+"flutter_bootstrap.js": "a0a8eef2230266a7016b40293e3108f2",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "fdb49c272c809c5e516a4b7ceefc5d06",
-"/": "fdb49c272c809c5e516a4b7ceefc5d06",
-"main.dart.js": "f6c190890294e2975f6996435e09e69f",
+"index.html": "6342839d9a2b26b3e1f1b9eeb7ca93b9",
+"/": "6342839d9a2b26b3e1f1b9eeb7ca93b9",
+"main.dart.js": "da9ef874118cbad40672b9397d777498",
 "manifest.json": "ade3eb452e66bdb456fb2c971a1e1c2a",
 "version.json": "322fd90e3e14d60766c92bf53e5845cf"};
 // The application shell files that are downloaded before a service worker can
