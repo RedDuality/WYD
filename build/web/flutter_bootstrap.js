9693f37452e2f64cdde79dee0c0bbd0348db6bdd
RedDuality <giacomo_romanini2000@libero.it>
2024-12-12 00:48:08 +0100

    build web

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2207592515"
+    serviceWorkerVersion: "1159578389"
   }
 });
