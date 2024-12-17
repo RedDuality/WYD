90e82b3fc196551a554ef4b1c069f2ee4ca32cab
RedDuality <giacomo_romanini2000@libero.it>
2024-12-17 10:31:46 +0100

    create community bug fix

--- lib/model/DTO/create_community_dto.dart
+++ lib/model/DTO/create_community_dto.dart
@@ -1,19 +1,18 @@
 
 import 'package:wyd_front/model/enum/community_type.dart';
-import 'package:wyd_front/model/profile.dart';
 
 class CreateCommunityDto {
   int id = 0;
   String name = "";
   CommunityType type = CommunityType.personal;
-  List<Profile> users = [];
+  List<int> profileIds = [];
 
   CreateCommunityDto({
     this.id = -1,
     this.name = "",
     this.type = CommunityType.personal,
-    List<Profile>? users,
-  }) : users = users ?? [];
+    List<int>? ids,
+  }) : profileIds = ids ?? [];
 
   factory CreateCommunityDto.fromJson(Map<String, dynamic> json) {
     return switch (json) {
@@ -21,12 +20,12 @@ class CreateCommunityDto {
         "id" : int id,
         "name" : String name,
         "type" : int? type,
-        "users" : List<dynamic>? users,
+        "profiles" : List<int>? profileIds,
       } => CreateCommunityDto(
         id : id, 
         name : name, 
         type : type != null ? CommunityType.values[type] : CommunityType.personal, 
-        users : users != null ? users.map((user) => Profile.fromJson(user as Map<String,dynamic>)).toList() : <Profile>[]
+        ids : profileIds ?? [],
         ),
       _ => throw const FormatException('Failed to decode community')
     };
@@ -37,7 +36,7 @@ class CreateCommunityDto {
       'id': id,
       'name': name,
       'type': type.index,
-      'users': users.map((user) => user.toJson()).toList(),
+      'profiles': profileIds,
     };
   }
 
