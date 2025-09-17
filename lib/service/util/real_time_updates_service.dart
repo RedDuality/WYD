966b8a812a91c459f29229fb4c2cc23f1305942b
RedDuality <giacomo_romanini2000@libero.it>
2025-09-17 12:34:03 +0200

    removed firestore

--- lib/service/util/real_time_updates_service.dart
+++ lib/service/util/real_time_updates_service.dart
@@ -1,11 +1,9 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:wyd_front/model/enum/update_type.dart';
 import 'package:wyd_front/service/event/event_view_service.dart';
 import 'package:wyd_front/service/media/media_service.dart';
 import 'package:wyd_front/service/event/event_service.dart';
 import 'package:wyd_front/state/event/event_provider.dart';
-import 'package:wyd_front/state/user/user_provider.dart';
 
 class RealTimeUpdateService {
   static final RealTimeUpdateService _instance =
@@ -15,37 +13,14 @@ class RealTimeUpdateService {
     return _instance;
   }
 
-  late String deviceId;
-  late DateTime creationTime;
 
   RealTimeUpdateService._internal();
 
-  bool firstread = true;
 
   Future<void> start() async {
-    var user = UserProvider().user;
-    if (user == null) throw "User is null";
-
-    creationTime = DateTime.now();
-
-    FirebaseFirestore.instance
-        .collection(user.hash)
-        .orderBy('timestamp', descending: true)
-        .limit(1)
-        .snapshots()
-        .listen((snapshot) {
-      if (firstread) {
-        firstread = false;
-        return;
-      }
-      var update = snapshot.docs[0];
-
-      //Yes, current device will update 2 times
-      handleUpdate(update);
-    });
   }
 
-  void handleUpdate(QueryDocumentSnapshot<Map<String, dynamic>> snapshot) {
+  void handleUpdate(var snapshot) {
     var typeIndex = snapshot['type'];
     switch (UpdateType.values[typeIndex]) {
       case UpdateType.newEvent:
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import cloud_firestore
 import file_picker
 import firebase_auth
 import firebase_core
@@ -20,7 +19,6 @@ import shared_preferences_foundation
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
